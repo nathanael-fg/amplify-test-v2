a1519c2fcf9a7115406cd769fcb415e8907fc38c
nathanael-fg <nathanael.fletcher@financialgravity.com>
2025-02-25 17:12:50 -0600

    Generating schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 
 // const client = generateClient<Schema>();
 // const {data } = await client.queries.getAccount({
