1a5914367676337937d3cb21b177ab217d424de9
nathanael-fg <nathanael.fletcher@financialgravity.com>
2025-02-25 09:59:45 -0600

    Updating import order for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
