77f7aab3d45bf5a31a4fc4a0c8c059f914f99d2f
nathanael-fg <nathanael.fletcher@financialgravity.com>
2025-02-25 09:35:17 -0600

    Adding placeholder client query

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
+const {data, errors } = await client.queries.getAccount({
+  id: "217174"
+});
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+  console.log(data)
+
+  // useEffect(() => {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
+  // }, []);
+
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.Todo.create({ content: window.prompt("Input Account Id") });
   }
 
     
@@ -24,15 +33,12 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>Account Info:</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
           <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}
+          key={data?.id}>{data?.name}
           </li>
-        ))}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
