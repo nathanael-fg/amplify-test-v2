59cfe5b4303f7c1d5b902142bf44fbdffc5b9e07
nathanael-fg <nathanael.fletcher@financialgravity.com>
2025-02-25 10:07:59 -0600

    Adding high level imports to getAccount.js

--- amplify/data/getAccount.js
+++ amplify/data/getAccount.js
@@ -1,4 +1,8 @@
 import * as ddb from "@aws-appsync/utils/dynamodb";
+import { Amplify } from "aws-amplify";
+import outputs from "../../amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 export function request(ctx) {
   return ddb.get({ key: { id: ctx.args.id } });
